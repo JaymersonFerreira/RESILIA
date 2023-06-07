09aa0b78e405fa3b8195ac0ca912cdd3510af3a5
Jaymerson <jaymerson.ferreira.s@gmail.com>
2023-06-07 17:05:23 -0300

    aula18.1

---
+++ modulo2/aula18/aula18.py
@@ -0,0 +1,13 @@
+#fila
+
+fila = [10, 20, 30, 40, 50]
+fila.append(60)
+print('fila.append(60): ',fila)
+fila.pop(0)
+print('fila.pop(0): ',fila)
+fila.append(70)
+print('fila.append(70): ',fila)
+fila.pop(0)
+print('fila.pop(0): ',fila)
+
+
